48eab5248725a7c3c1c6af1a0e12f038a3916af9
Zobayer26 <zobayerarif126@gmail.com>
2024-02-11 11:32:23 +0600

    show task

--- my-app/src/components/ShowTask.tsx
+++ my-app/src/components/ShowTask.tsx
@@ -1,3 +1,6 @@
+import { MdDelete } from "react-icons/md";
+import { FaEdit } from "react-icons/fa";
+
 type ShowtaskType = {
   task: any;
   setTask: any;
@@ -7,9 +10,23 @@ const ShowTask: React.FC<ShowtaskType> = ({ task, setTask }) => {
     <div className="flex flex-col gap-3">
       <ul className="flex flex-col  gap-2 px-2">
         {task.map((item: any) => (
-          <li className="flex items-center justify-between px-1 " key={item.id}>
-            {item.value}
-            {item.priority}
+          <li
+            className="p-1 border rounded hover:border-orange-500
+          flex items-center justify-between px-1 "
+            key={item.id}
+          >
+            <div className="flex gap-1">
+              <p>{item.value}</p>
+              <p>{item.priority}</p>
+            </div>
+            <div className="text-red-500  flex items-center gap-2">
+              <div className="hover:text-orange-300">
+                <FaEdit />
+              </div>
+              <div className="hover:text-orange-300">
+                <MdDelete />
+              </div>
+            </div>
           </li>
         ))}
       </ul>
